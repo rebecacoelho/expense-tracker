fe8cae39410ec35930d949a81a6bc9c67a5a8839
Rebeca Coelho <rebecasantoscoelho@gmail.com>
2022-10-20 10:45:39 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item'
+
+export const items: Item[] = [
+    { date: new Date(2022, 9, 6), category: 'food', title: "McDonalds", value: 32.12 },
+    { date: new Date(2022, 9, 15), category: 'food', title: "Buguer King", value: 28 },
+    { date: new Date(2022, 9, 16), category: 'rent', title: "Aluguel App", value: 2300 },
+    { date: new Date(2022, 11, 18), category: 'salary', title: "Salário ACME", value: 4200 }
+];
